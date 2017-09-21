db9635bc015171e0fe42806ce930af623b7c8f2f
Sean Doyle <sean.p.doyle24@gmail.com>
2017-09-21 11:56:27 -0400

    fixup!

--- test/features/user-visits-root.js
+++ test/features/user-visits-root.js
@@ -1,9 +1,9 @@
-const assert = require('assert');
+const assert = require("assert");
 
-describe('User visits root', () => {
-  it('displays a welcome message', () => {
-    browser.url('/');
+describe("User visits root", () => {
+  it("displays a welcome message", () => {
+    browser.url("/");
 
-    assert.equal(browser.getText('[data-role="welcome"]'), 'Welcome to Express');
+    assert.equal(browser.getText('[data-role="welcome"]'), "Welcome to Express");
   });
 });
