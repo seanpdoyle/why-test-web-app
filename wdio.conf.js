e13d1e67d3e901622c2917ca7c39efed6fa7eeb4
Sean Doyle <sean.p.doyle24@gmail.com>
2017-09-21 11:40:58 -0400

    Initial Commit
    This commit introduces a scaffolded Express application with
    browser-level test coverage through a headless Chrome process, driven
     by [WebDriverIO][wdio].
    
    [wdio]: http://webdriver.io/

---
+++ wdio.conf.js
@@ -0,0 +1,28 @@
+const app = require('./app');
+const port = process.env.EXPRESS_PORT || 3000;
+
+let expressServer;
+
+exports.config = {
+  specs: [
+    'test/features/*.js',
+  ],
+  coloredLogs: true,
+  baseUrl: `http://localhost:${port}/public`,
+  framework: 'mocha',
+  reporters: ['dot'],
+  capabilities: [{
+    browserName: 'chrome',
+  }],
+  chromeOptions: {
+    args: ['--headless'],
+  },
+  services: ['selenium-standalone'],
+
+  onPrepare() {
+    expressServer = app.listen(port);
+  },
+  onComplete() {
+    expressServer.close();
+  }
+}
