b2c70f3bbcfb6da2db26d64dfc68c7711c61fdde
Ian Munro <ianmunrobot@gmail.com>
2017-10-24 13:38:34 -0400

    Updates formatting to hew a little closer to explicit + emergent CC style
    - prefer singlequotes
    - prefer const
    - prefer chained property access on newline instead of end of line
    - prefer google styleguide spacing
    - adds eslintrc file extending Google's JS styleguide and overwrites some
    - Did not add `eslint` as a project dependency for now since we're not using it in build system. I'd recommend installing it globally, but I am happy to change this

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-var express = require("express");
-var expressHandlebars  = require("express-handlebars");
-var path = require("path");
-var favicon = require("serve-favicon");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
-const database = require("./database");
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require("./routes/index");
-const messages = require("./routes/messages");
+const database = require('./database');
+const index = require('./routes/index');
+const messages = require('./routes/messages');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.engine("handlebars", expressHandlebars({ defaultLayout: "app" }));
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "handlebars");
+// View engine setup
+app.engine('handlebars', expressHandlebars({defaultLayout: 'app'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-app.use(logger("dev"));
+// Uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", index);
-app.use("/messages", messages);
+app.use('/', index);
+app.use('/messages', messages);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error("Not Found");
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// Error handler
 app.use((err, req, res) => {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Message = require("../models/message");
 
-router.get("/", async (req, res) => {
+const Message = require('../models/message');
+
+router.get('/', async (req, res) => {
   const messages = await Message.find({});
 
-  res.render("index", { messages });
+  res.render('index', {messages});
 });
 
 module.exports = router;
