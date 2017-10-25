673f5cfa242ce76c3aa67d1bd4c3f2067a622ed5
Sean Doyle <sean.p.doyle24@gmail.com>
2017-10-25 15:37:04 -0400

    WIP Using <script> tags

---
+++ public/App.jsx
@@ -0,0 +1,10 @@
+class App extends React.Component {
+  render() {
+    return <div>Something something Dark Side</div>;
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
