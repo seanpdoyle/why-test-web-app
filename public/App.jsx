6122a795e5a50d1c90b4d155aedc164d9928c2ac
Sean Doyle <sean.p.doyle24@gmail.com>
2017-10-25 15:53:02 -0400

    WIP [skip ci] [ci skip]

--- public/App.jsx
+++ public/App.jsx
@@ -1,6 +1,16 @@
 class App extends React.Component {
   render() {
-    return <div>Something something Dark Side</div>;
+    return (
+      <form action="/messages" method="post" data-role="message-form">
+      <label for="author">Your name:</label>
+      <input id="author" name="author" />
+
+      <label for="message">Your message:</label>
+      <input id="message" name="message" />
+
+      <input type="submit">
+      </form>
+    )
   }
 }
 
