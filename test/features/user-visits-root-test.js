6957e5b922d8a40fe20dc3e9b47268c74a49d6eb
nduckwiler <nicholasduckwiler@Nicholass-MacBook-Pro.local>
2017-10-25 16:52:50 -0400

    Rollback template, short circuit test

--- test/features/user-visits-root-test.js
+++ test/features/user-visits-root-test.js
@@ -1,20 +1,15 @@
 const {assert} = require('chai');
 
-const messagesText = () => browser.getText('#messages');
-
 describe('User visits root', () => {
-  describe('posting a message', () => {
-    it('saves the message with the author information', () => {
-      const author = 'Inquisitive User';
-      const message = 'Why test?';
+  describe('to post an order', () => {
+    it('accepts the customer name', () => {
+      const name = 'Hungry Person';
 
       browser.url('/');
-      browser.setValue('[name=author]', author);
-      browser.setValue('[name=message]', message);
-      browser.click('[type=submit]');
+      browser.setValue('#name', name);
+      browser.click('#submit-name');
 
-      assert.include(messagesText(), message);
-      assert.include(messagesText(), author);
+      assert.include(browser.getText('#deliver-to'), name);
     });
   });
 });
