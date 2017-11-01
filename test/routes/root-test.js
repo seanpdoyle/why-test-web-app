37ee84ce855faad26d111fcf64e592a307af46c2
nduckwiler <nicholasduckwiler@Nicholass-MacBook-Pro.local>
2017-11-01 16:22:35 -0400

    refactor route tests

--- test/routes/root-test.js
+++ test/routes/root-test.js
@@ -30,21 +30,13 @@ describe('/', () => {
 
     describe('when the Order already exists', () => {
       it('renders the Order', async () => {
-        const order = await Order.create({ name: 'Hungry User' })
+        const name = 'Hungry User'
+        const order = await Order.create({name})
 
         const response = await request(server).get('/');
 
         assert.equal(response.status, 200);
-        assert.include(response.text, 'Hungry User');
-      });
-
-      it('renders the Order', async () => {
-        const order = await Order.create({ name: 'Hungrier User' })
-
-        const response = await request(server).get('/');
-
-        assert.equal(response.status, 200);
-        assert.include(response.text, 'Hungrier User');
+        assert.include(response.text, name);
       });
     });
   });
@@ -64,22 +56,22 @@ describe('/', () => {
     });
 
     it('updates the name on the order', async () => {
-      const name1 = 'Inquisitive User';
-      const name2 = 'Anxious User';
+      const initialName = 'Inquisitive User';
+      const updatedName = 'Anxious User';
 
       await request(server)
         .post('/')
-        .send({name: name1});
+        .send({name: initialName});
 
       const response = await request(server)
         .post('/')
-        .send({name: name2});
+        .send({name: updatedName});
       
       assert.equal(response.status, 200);
-      assert.include(response.text, name2);
-      assert.notInclude(response.text, name1);
+      assert.include(response.text, updatedName);
+      assert.notInclude(response.text, initialName);
       const order = await Order.findOne();
-      assert.equal(order.name, name2);
+      assert.equal(order.name, updatedName);
     });
   });
 });
