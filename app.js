10ce7491cce03eb175571dd30c8230eee95647d8
nduckwiler <nduckwiler@gmail.com>
2017-11-15 17:59:02 -0500

    Remove logger and add cursor pointer for usability

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.engine('handlebars', expressHandlebars({defaultLayout: 'app'}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
